25aa87a3a7c1068911520b252dfcf60f13ba59ad
Nikolett Jávor <nikolettjavor@gmail.com>
2023-09-11 11:04:04 +0200

    első tanítási nap

---
+++ 1_grafok/grafok-allatkert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace grafok_allatkert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sor = Console.ReadLine().Split(' ');            
+            int N = int.Parse(sor[0]);
+            int M = int.Parse(sor[1]);
+
+
+            int[][] pontok = new int[N][];
+            foreach (var item in pontok)
+            {
+                item = 0;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                sor = Console.ReadLine().Split(' ');
+                pontok[int.Parse(sor[0])] ++;
+                pontok[int.Parse(sor[1])]++;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# SZLG12F
-SZLG 2023-24-es tanév
+# SZLG 12F (2023-24)
+
+## Témakörök
+1. [Gráfok](1_grafok)
+
+## Házi feladatok
+| Témakör | Feladat | /100 |
+| :---: | :---: | :---: |
+
+## Órai feladatok
+### 1. [Gráfok](1_grafok)
+- [Állatkert](1_grafok/feladatok/1_allatkert/Program.cs)
