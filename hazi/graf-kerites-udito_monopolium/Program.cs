14e305bd69a09ccb90587e6dfb9e385409fbb97f
Nikolett Jávor <nikolettjavor@gmail.com>
2024-01-12 14:19:03 +0100

    eehhh

--- hazi/graf-kerites-udito_monopolium/Program.cs
+++ hazi/graf-kerites-udito_monopolium/Program.cs
@@ -1,117 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static List<int>[] graph;
-    static int[] drinks;
-
-    static int BFS(int start)
+    static void Main()
     {
-        Queue<int> queue = new Queue<int>();
-        queue.Enqueue(start);
-
-        int[] visited = new int[drinks.Length];
-        visited[start] = 1;
-
-        int targetDrink = drinks[start];
-        int weeks = 0;
-
-        while (queue.Count > 0)
-        {
-            public bool italpreferencia; //Calo (0) vagy Pipse (1)
-            public List<int> baratok;
-
-            while (count > 0)
-            {
-                int current = queue.Dequeue();
-
-            public void Diagnosztika()
-            {
-                Console.WriteLine("\n\n-----------------------\nDiagnosztika - bergeng\n- - - - - - - - - - - -\n");
-                for (int i = 0; i < baratok.Count(); i++)
-                    Console.Write($"[{i} - ital:{italpreferencia}]: [{String.Join(", ", baratok[i])}]\n");
-                Console.WriteLine("\n-----------------------\n");
-            }
-
-            public void Italvaltas() => italpreferencia = !italpreferencia;
-        }
-        class Teszteset
-        {
-            List<Bergengoc> bergengoclista;
-            (int C, int P) = (0,0);
-
-            public Teszteset(int N, int M)
-            {
-                bergengoclista = new List<Bergengoc>();
-                string[] sor = Console.ReadLine().Split(' ');
-                for (int i = 0; i < N; i++)
-                    bergengoclista.Add(new Bergengoc(int.Parse(sor[i])));
-
-                for (int j = 0; j < M; j++)
-                {
-                    if (visited[neighbor] == 0)
-                    {
-                        visited[neighbor] = 1;
-                        queue.Enqueue(neighbor);
-
-                        if (drinks[neighbor] != targetDrink)
-                        {
-                            drinks[neighbor] = targetDrink;
-                        }
-                    }
-                }
-
-                count--;
-            }
-
-            public void Diagnosztika()
-            {
-                Console.WriteLine("\n\n-----------------------\nDiagnosztika\n- - - - - - - - - - - -\n");
-                for (int i = 0; i < bergengoclista.Count(); i++)
-                {
-                    Console.Write($"[{i} - ital:{bergengoclista[i].italpreferencia}]: [{String.Join(", ", bergengoclista[i].baratok)}]\n");
-                }
-                Console.WriteLine("\n-----------------------\n");
-            }
-
-            
-        }
-
-        return weeks;
-    }
-
-    static void Main(string[] args)
-    {
-        int T = int.Parse(Console.ReadLine());
+        int T = int.Parse(Console.ReadLine()); // tesztesetek száma
+        int[] results = new int[T];
 
         for (int t = 0; t < T; t++)
         {
             string[] input = Console.ReadLine().Split();
-            int N = int.Parse(input[0]);
-            int M = int.Parse(input[1]);
+            int N = int.Parse(input[0]); // bergengócok száma
+            int M = int.Parse(input[1]); // baráti kapcsolatok száma
 
-            drinks = new int[N];
-            graph = new List<int>[N];
+            int[] preferences = Console.ReadLine().Split().Select(int.Parse).ToArray(); // Bergengócok kezdeti preferenciái
 
-            for (int i = 0; i < N; i++)
-            {
-                drinks[i] = int.Parse(Console.ReadLine());
+            List<int>[] graph = new List<int>[N + 1];
+            for (int i = 1; i <= N; i++)
                 graph[i] = new List<int>();
-            }
 
             for (int i = 0; i < M; i++)
             {
-                input = Console.ReadLine().Split();
-                int u = int.Parse(input[0]) - 1;
-                int v = int.Parse(input[1]) - 1;
-
-                graph[u].Add(v);
-                graph[v].Add(u);
+                int[] edge = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                graph[edge[0]].Add(edge[1]);
+                graph[edge[1]].Add(edge[0]);
             }
 
-            int result = BFS(0);
-            Console.WriteLine(result);
+            int[] weeksNeeded = GetWeeksNeeded(N, preferences, graph);
+            results.Append(weeksNeeded.Max());
         }
+        foreach (int res in results)
+        {
+            Console.WriteLine(res);
+        }
+    }
+
+    static int[] GetWeeksNeeded(int N, int[] preferences, List<int>[] graph)
+    {
+        int[] weeksNeeded = new int[N + 1];
+        Queue<int> queue = new Queue<int>();
+        bool[] visited = new bool[N + 1];
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (!visited[i])
+            {
+                visited[i] = true;
+                queue.Enqueue(i);
+
+                while (queue.Count > 0)
+                {
+                    int current = queue.Dequeue();
+
+                    foreach (int neighbor in graph[current])
+                    {
+                        if (!visited[neighbor])
+                        {
+                            visited[neighbor] = true;
+                            preferences[neighbor - 1] = preferences[current - 1];
+                            queue.Enqueue(neighbor);
+                            weeksNeeded[neighbor] = weeksNeeded[current] + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return weeksNeeded;
     }
 }
