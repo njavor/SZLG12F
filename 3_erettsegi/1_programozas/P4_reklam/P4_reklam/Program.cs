ad4da0f9c9515f964eab0ea3b9f18063e53e348a
njavor <nikolettjavor@gmail.com>
2024-03-20 12:17:22 +0100

    24.03.20 - 2

---
+++ 3_erettsegi/1_programozas/P4_reklam/P4_reklam/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace P4_reklam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> adatok = new List<string[]>();
+
+            Console.WriteLine("kezdés");
+            using (StreamReader r = new StreamReader("rendel.txt", Encoding.UTF8))
+            {
+                string[] line;
+                while (!r.EndOfStream)
+                {
+                    adatok.Add(new string[3](r.ReadLine().Split(' ')));
+                    adatok[-1] = r.ReadLine().Split(' ');
+                }
+            }
+
+            // 2.
+            Console.WriteLine(adatok.Count());
+        }
+    }
+}
